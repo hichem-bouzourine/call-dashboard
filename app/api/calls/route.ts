be2f5235e93ace775b8113e50883c1b01fa5f4fa
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-04-12 00:34:41 +0200

    Backend - enrich Calls API.

--- app/api/calls/route.ts
+++ app/api/calls/route.ts
@@ -5,12 +5,19 @@ export const GET = async (req: Request, res: Response) => {
   const url = new URL(req.url, 'http://localhost:3000');
   const toValue = url.searchParams.get('to')?.trim();
 
-  // Construct complete phone number (add leading '+' if needed)
-  const correctNumber = "+" + (toValue || "");
+  if (toValue !== undefined ) {
 
-  // Filter calls based on 'to' number
-  const filteredCalls = toValue ? callsData.filter(call => call.to === correctNumber) : [];
-
-  // Respond with JSON-encoded filtered calls (status 200)
-  return new Response(JSON.stringify(filteredCalls), { status: 200 });
+    // Construct complete phone number (add leading '+' if needed)
+    const correctNumber = "+" + (toValue || "");
+    
+    // Filter calls based on 'to' number
+    const filteredCalls = toValue ? callsData.filter(call => call.to === correctNumber) : [];
+    
+    // Respond with JSON-encoded filtered calls (status 200)
+    return new Response(JSON.stringify(filteredCalls), { status: 200 });
+  } else {
+    
+    // Respond with JSON-encoded calls (status 200)
+    return new Response(JSON.stringify(callsData), { status: 200 });
+  }
 };
